d69af9ea931cd787977b5e369e8cf0f6a5510d04
king-o-hill <kohidevice@gmail.com>
2025-05-22 22:10:38 -0400

    Final provisioning updates

--- first_run.sh
+++ first_run.sh
@@ -13,20 +13,22 @@ else
   git clone --quiet "$REPO_URL" "$CLONE_DIR"
 fi
 
+sleep 3
+
+echo "🔧 Setting permissions on all scripts..."
+chmod +x "$CLONE_DIR"/*.sh
+
 # Remove broken symlinks if they exist
-for cmd in newct destroyct users; do
+for cmd in newct destroyct; do
     LINK="/usr/local/bin/$cmd"
     [ -L "$LINK" ] && [ ! -e "$LINK" ] && rm "$LINK"
 done
 
 sleep 3
 
-echo "🔧 Setting permissions on all scripts..."
-chmod +x "$CLONE_DIR"/*.sh
-
 echo "🔗 Creating or fixing symlinks..."
 ln -sf "$CLONE_DIR/create_container.sh" /usr/local/bin/newct
-ln -sf "$CLONE_DIR/destroy_container.sh" /usr/local/bin/destroyct
+ln -sf "$CLONE_DIR/destroyct.sh" /usr/local/bin/destroyct
 
 sleep 3
 
